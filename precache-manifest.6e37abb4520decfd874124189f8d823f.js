08afd58e56e0a4f1db38688a326a2c5adc8a898b
romainfrezier <romainfrezier@gmail.com>
2022-11-01 16:25:21 +0100

    new image

---
+++ precache-manifest.6e37abb4520decfd874124189f8d823f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8700c83b40031b0dacb5",
+    "url": "/static/css/main.9e9b7d9d.chunk.css"
+  },
+  {
+    "revision": "8700c83b40031b0dacb5",
+    "url": "/static/js/main.8700c83b.chunk.js"
+  },
+  {
+    "revision": "359480c151718051aadd",
+    "url": "/static/js/1.359480c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c336db7d5bd72819e4c0a4a700ff5a9",
+    "url": "/index.html"
+  }
+];
