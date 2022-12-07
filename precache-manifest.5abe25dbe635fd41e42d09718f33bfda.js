30e0f6b4384c382d74b3f19afcc22ebde2ea8ac7
romainfrezier <romainfrezier@gmail.com>
2022-12-07 10:17:04 +0100

    Add skills on slider

---
+++ precache-manifest.5abe25dbe635fd41e42d09718f33bfda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "160c4062b0a99f141c70",
+    "url": "/static/css/main.fbb70e72.chunk.css"
+  },
+  {
+    "revision": "160c4062b0a99f141c70",
+    "url": "/static/js/main.160c4062.chunk.js"
+  },
+  {
+    "revision": "9ca2236321d71bdd0b08",
+    "url": "/static/css/1.7d1fb35a.chunk.css"
+  },
+  {
+    "revision": "9ca2236321d71bdd0b08",
+    "url": "/static/js/1.9ca22363.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d218cc3794ace2b4e477ca07edece315",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.991b8a8e98ac818e46a4d033a51342e9.js"
+  "/precache-manifest.5abe25dbe635fd41e42d09718f33bfda.js"
 );
 
 workbox.clientsClaim();
